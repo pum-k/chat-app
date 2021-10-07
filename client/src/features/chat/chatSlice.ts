0d950a92c0b65f056092336c8eb879c0a01bc821
ezzio <thangduong1233@gmail.com>
2021-10-07 09:28:11 +0700

    login access_token

--- client/src/features/chat/chatSlice.ts
+++ client/src/features/chat/chatSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from 'app/store';
 import { ChatState, messageStructure } from 'constants/ChatTypes';
-
+import io from 'socket.io-client';
 const initialState: ChatState = {
   messages: [],
 };
