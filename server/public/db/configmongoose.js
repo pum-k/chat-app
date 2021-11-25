f8d8d8cef4e25a383bcb134bb4679690cbd5bf3f
Dang Khoa Duong <ddkhoa1206@gmail.com>
2021-11-25 17:18:17 +0700

    chore: remove coment

--- server/public/db/configmongoose.js
+++ server/public/db/configmongoose.js
@@ -5,13 +5,9 @@ module.exports = async function connection() {
     .connect(
       process.env.MONGODB_URL ||
         'mongodb+srv://administrator:administrator@chatdb.oyaqd.mongodb.net/chatdb?retryWrites=true&w=majority',
-        // "mongodb://localhost:27017/chat-app",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        // useCreateIndex: true,
-        useUnifiedTopology: true,
-        // useFindAndModify: true,
       }
     )
     .catch((err) => console.log(err));
